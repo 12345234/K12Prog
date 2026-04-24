747b397157313b2c001e5af6f95fafd724b31a19
raytrek2-\ktc <>
2026-04-24 12:29:45 +0900

    player二段ジャンプ禁止

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -9,11 +9,18 @@ public class Player : MonoBehaviour
     PlayerInput playerInput;
     Rigidbody2D rb;
 
+
+    public LayerMask Ground;
+    public LayerMask Enemy;
+
+    RaycastHit2D hit;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
@@ -23,9 +30,28 @@ public class Player : MonoBehaviour
 
         rb.linearVelocityX = move.x * speed;
 
-        if (playerInput.actions["Jump"].WasPressedThisFrame())
+        if (playerInput.actions["Jump"].WasPressedThisFrame()&&JumpControl())
         {
             rb.linearVelocityY = jumpSpeed;
         }
+
+        if(JumpAttack())
+        {
+            Debug.Log("aa");
+        }
+
+        Debug.DrawRay(transform.position, Vector2.down, Color.red);
+    }
+
+    private bool JumpControl()
+    {
+        hit = Physics2D.Raycast(transform.position, Vector2.down, 1.0f, Ground);
+        return hit.collider !=null;
+    }
+
+    private bool JumpAttack()
+    {
+        hit = Physics2D.Raycast(transform.position, Vector2.down, 1.0f, Enemy);
+        return hit.collider !=null;
     }
 }
